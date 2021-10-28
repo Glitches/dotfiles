d981043bf672e01d6ddaf95296a630f7ea7109f5
Andrea <gltch2003@gmail.com>
2021-10-28 08:39:56 +0200

    feature: buftabline uses buffer index

--- nvim/lua/plugins/buftabline.lua
+++ nvim/lua/plugins/buftabline.lua
@@ -1,8 +1,31 @@
 local buftabline = require("buftabline")
-
 local u = require("utils")
 
-buftabline.setup()
+local options = {
+	tab_format = " #{n}: #{b}#{f} #{i} ",
+	buffer_id_index = true,
+	icon_colors = true,
+	start_hidden = false,
+	auto_hide = false,
+	disable_commands = false,
+	go_to_maps = true,
+	flags = {
+		modified = "[+]",
+		not_modifiable = "[-]",
+		readonly = "[RO]",
+	},
+	hlgroups = {
+		current = "TabLineSel",
+		normal = "TabLine",
+		active = nil,
+		spacing = nil,
+		modified_current = nil,
+		modified_normal = nil,
+		modified_active = nil,
+	},
+}
+
+buftabline.setup(options)
 
 buftabline.map({ prefix = "<Leader>c", cmd = "bdelete" })
 buftabline.map({ prefix = "<Leader>v", cmd = "vertical sb" })
