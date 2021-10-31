c3ae201bc124ddfd0c1f5de6e90aeb56486c2575
Andrea <gltch2003@gmail.com>
2021-10-31 09:41:49 +0100

    chore: move on the left

--- nvim/lua/plugins/nvimtree.lua
+++ nvim/lua/plugins/nvimtree.lua
@@ -59,12 +59,8 @@ nvim_tree.setup {
     },
 
     view = {
-        -- width of the window, can be either a number (columns) or a string in `%`, for left or right side placement
-        width = 40,
-        -- height of the window, can be either a number (columns) or a string in `%`, for top or bottom side placement
-        height = 30,
-        -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
-        side = 'right',
+        -- width of the window, can be either a number (columns) or a string in `%`, for left or right' | 'top' | 'bottom'
+        side = 'left',
         -- if true the tree will resize itself after opening a file
         auto_resize = true,
         mappings = {
