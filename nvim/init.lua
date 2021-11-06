afb34a0e778596df469c2327eac63194bd2c8cbb
Andrea <gltch2003@gmail.com>
2021-11-06 09:46:57 +0100

    feature: use absolute line numbers

--- nvim/init.lua
+++ nvim/init.lua
@@ -22,7 +22,7 @@ vim.opt.scrolloff = 4
 vim.opt.sidescrolloff = 2
 vim.opt.cursorline = true
 vim.opt.number = true
-vim.opt.relativenumber = true
+-- vim.opt.relativenumber = true
 vim.opt.signcolumn = "yes"
 vim.opt.timeoutlen = 300
 vim.opt.shortmess:append("cA")
